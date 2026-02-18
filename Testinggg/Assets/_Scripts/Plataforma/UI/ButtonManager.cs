2a9c2aac17aa057ccf6736c1a4e7aee624406d18
HHCH24 <helensofiachavera@gmail.com>
2026-02-18 12:25:28 -0500

    Clase 18 febrero

---
+++ Testinggg/Assets/_Scripts/Plataforma/UI/ButtonManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonManager : MonoBehaviour
+{
+public void NewGame()
+    {
+        Debug.Log("Inicia un nuevo juego");
+    }
+public void Continue()
+    {
+        Debug.Log("Continua el juego");
+    }
+ public void Options()
+    {
+        Debug.Log("Ver opciones");
+    }
+}
